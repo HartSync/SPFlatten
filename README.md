a22403fac2dcc66f5af847c35bba470f1479a7d7
daidokoro <shaun@daidokoro.io>
2017-01-02 13:35:51 +0000

    Initial Commit of Revamped Code base

--- README.md
+++ README.md
@@ -3,10 +3,81 @@ SPFlatten
 
 Flatten SPF records
 
-The Sender Policy Framework has a limit as to how many DNS calls a SPF client can make before the protocol fails open. Many large email providers have a hard time coming under that 10 DNS call limit due to the complex nature of their email sending infrastructure. This script will recursively resolve the SPF records for a given domain, and flatten the INCLUDE, A, and MX records down to their respective IP4 or IP6 addresses. This will drastically reduce the DNS calls to come in under the SPF limit. Only compatible with SPFv1, but SPFv2 compatability would be easy to add.
+The Sender Policy Framework has a limit as to how many DNS calls a SPF client can make before the protocol fails open. Many large email providers have a hard time coming under that 10 DNS call limit due to the complex nature of their email sending infrastructure. 
 
-Usage:
-Edit the "root_domain" variable to be whatever domain you want the SPF record flattened for, and run the script. 
+This script is based on the Original Project by Nops @https://github.com/0x9090
+
+This script will recursively resolve the SPF records for a given domain, and flatten the INCLUDE, A, and MX records down to their respective IP4 or IP6 addresses, then further splits the output in sub-domains based on the UDP size limit. This will drastically reduce the DNS calls to come in under the SPF limit. Only compatible with SPFv1.
+
+
+CLI Usage Example:
+====================
+    ./SPFlatten.py google.com
+
 
 Output:
-The program will echo out its progress, and give you a final Flattened SPF record - which should be mostly IP4 and IP6 addresses.
+-------
+
+    google.com:
+
+    v=spfv1 ip4:66.249.80.0/20 ip6:2c0f:fb50:4000::/36 ip4:209.85.128.0/17 ip4:64.18.0.0/20 ip4:207.126.144.0/20 ip4:216.58.192.0/19 ip6:2800:3f0:4000::/36 ip4:108.177.8.0/21 ip4:173.194.0.0/16 ip4:64.233.160.0/19 ip4:216.239.32.0/19 ip6:2001:4860:4000::/36 ip6:2404:6800:4000::/36 ip6:2607:f8b0:4000::/36 ip6:2a00:1450:4000::/36 ip4:74.125.0.0/16 include:spf1.google.com ~all
+
+
+    spf1.google.com:
+
+    v=spf1 ip4:72.14.192.0/18 ip4:66.102.0.0/20 ip4:172.217.0.0/19 ~all
+
+
+
+For Json Output use:
+
+    ./SPFlatten.py google.com -j 
+
+Output:
+-------
+
+    '{"google.com": "v=spfv1 ip4:66.102.0.0/20 ip4:74.125.0.0/16 ip4:173.194.0.0/16 ip4:207.126.144.0/20 ip4:64.18.0.0/20 ip6:2a00:1450:4000::/36
+    ip4:172.217.0.0/19 ip6:2c0f:fb50:4000::/36 ip6:2800:3f0:4000::/36 ip4:64.233.160.0/19 ip6:2001:4860:4000::/36 ip4:216.58.192.0/19 ip6:2404:68
+    00:4000::/36 ip4:209.85.128.0/17 ip6:2607:f8b0:4000::/36 ip4:108.177.8.0/21 ip4:216.239.32.0/19 ip4:66.249.80.0/20 include:spf1.google.com ~a
+    ll", "spf1.google.com": "v=spf1 ip4:72.14.192.0/18 ~all"}'
+
+
+Python Class Usage Example:
+============================
+
+    from SPFlatten import SPFlatten
+
+    spf = SPFlatten(root_domain='google.com')
+    
+    spf.Outputs()
+
+Output:
+-------
+
+    google.com:
+
+    v=spfv1 ip4:66.249.80.0/20 ip6:2c0f:fb50:4000::/36 ip4:209.85.128.0/17 ip4:64.18.0.0/20 ip4:207.126.144.0/20 ip4:216.58.192.0/19 ip6:2800:3f0:4000::/36 ip4:108.177.8.0/21 ip4:173.194.0.0/16 ip4:64.233.160.0/19 ip4:216.239.32.0/19 ip6:2001:4860:4000::/36 ip6:2404:6800:4000::/36 ip6:2607:f8b0:4000::/36 ip6:2a00:1450:4000::/36 ip4:74.125.0.0/16 include:spf1.google.com ~all
+
+
+    spf1.google.com:
+
+    v=spf1 ip4:72.14.192.0/18 ip4:66.102.0.0/20 ip4:172.217.0.0/19 ~all
+
+
+
+SPFlatten - AWS Lambda
+======================
+
+A Lambda version of this project sits on the **spflatten_lambda** branch.
+
+**Usage:**
+
+_Checkout_ spflatten_lambda
+
+_Open_ **main.py** & _Set_ **domain** variable to your preferred domain. **NOTE: ** The Domain must exist on Route53 of the AWS account you are running lambda.
+
+    zip -r9 lambda.zip ./project_dir/*
+
+_Configure Lambda Function Using_: **main.handler**
+
+**NOTE:** This function will create TXT records containing flattened SPFs. If a record of the same name and similiar content exists, it will be overwritten.
--- SPFlatten.py
+++ SPFlatten.py
@@ -1,133 +1,186 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import re, dns.resolver
+import json
+from sys import getsizeof, argv
 
-#-----------------------------------------------------
-# SPFlattener - Because who needs limits??
-# Requires: dnspython
-# Usage: edit the "root_domain" variable below and run
-#-----------------------------------------------------
-# To-do:
-#     Confirm that SPF doesn't follow CNAMES (I don't think it does)
-#     Should we consider Sender ID? ie spf2.0 (probably not)
 
-#---------------------------------
-root_domain = "google.com"
-#---------------------------------
+class SPFlatten:
+   def __init__(self, root_domain='google.com', verbose=False, json_output=False):
+      self.root_domain = root_domain
+      self.spf_ip_list = []
+      self.spf_nonflat_mechanisms = []
+      self.all_mechanism = ''
+      self.split_records = {}
+      self.verbose = verbose
+      self.json_output = json_output
+      self.output = lambda *args: print(*args) if self.verbose else None
 
-spf_ip_list = []
-spf_nonflat_mechanisms = []
+      # Run Class
+      self.flatten()
+      self.structure_spf()
 
-def main():
-   flatten_spf(root_domain)
-
-   dedupe_spf_ip_list = list(set(spf_ip_list))
+      # Reconcile/Structure records.
+      self.spf_records = { x:y+' ~all' for x,y in self.split_records.items() if '~all' not in y }
+      self.spf_records[root_domain] = self.split_records[root_domain]
+      self.add_includes()
    
-   flat_spf = "v=spfv1"
-   for ip in dedupe_spf_ip_list:
-      if re.match(r'.*:.*', ip):
-         flat_spf += (" ip6:" + ip)
-      else:
-         flat_spf += (" ip4:" + ip)
+   def parse_mechanism(self, mechanism, domain):
+   # Parse the given mechansim, and dispatch it accordintly
+      if re.match(r'^a$', mechanism):
+         self.convert_domain_to_ipv4(domain)
+      elif re.match(r'^mx$', mechanism):
+         self.convert_mx_to_ipv4(domain)
+      elif re.match(r'^a:.*$', mechanism):
+         match = re.match(r'^a:(.*)$', mechanism)
+         self.convert_domain_to_ipv4(match.group(1))
+      elif re.match(r'^ip4:.*$', mechanism):
+         match = re.match(r'^ip4:(.*)$', mechanism)
+         self.output("IPv4 address found for", domain, ":", match.group(1))
+         self.spf_ip_list.append(match.group(1))
+      elif re.match(r'^ip6:.*$', mechanism):
+         match = re.match(r'^ip6:(.*)$', mechanism)
+         self.output("IPv6 address found for", domain, ":", match.group(1))
+         self.spf_ip_list.append(match.group(1))
+      elif re.match(r'^ptr.*$', mechanism):
+         self.output("PTR found for", domain, ":", mechanism)
+         self.spf_nonflat_mechanisms.append(mechanism)
+      elif re.match(r'^exists:$', mechanism):
+         self.output("Exists found for", domain, ":", mechanism)
+         self.spf_nonflat_mechanisms.append(mechanism)
+      elif re.match(r'^redirect:$', mechanism):
+         self.output("Redirect found for", domain, ":", mechanism)
+         self.spf_nonflat_mechanisms.append(mechanism)
+      elif re.match(r'^exp:$', mechanism):
+         self.output("EXP found for", domain, ":", mechanism)
+         self.spf_nonflat_mechanisms.append(mechanism)
+      elif re.match(r'^.all$', mechanism):
+         if domain == self.root_domain:
+            match = re.match(r'^(.all)$', mechanism)
+            self.output("All found for", domain, ":", match.group(1))
+            self.all_mechanism = " " + str(match.group(1))
+      elif re.match(r'^include:.*$', mechanism):
+         match = re.match(r'^include:(.*)', mechanism)
+         self.flatten_spf(match.group(1)) # recursion
 
-   for mechanism in spf_nonflat_mechanisms:
-      flat_spf += mechanism
+   def convert_domain_to_ipv4(self, domain):
+   # Convert A/AAAA records to IPs and adds them to the SPF master list
+      a_records = []
+      aaaa_records = []
 
-   flat_spf += all_mechanism
+      try:
+         a_records = dns.resolver.query(domain, "A")
+         for ip in a_records:
+            self.output("A record for", domain, ":", str(ip))
+            self.spf_ip_list.append(str(ip))
+      except dns.exception.DNSException:
+         pass
 
-   print "\nFlattened SPF:\n----------------------\n", flat_spf
+      try:
+         aaaa_records = dns.resolver.query(domain, "AAAA")
+         for ip in aaaa_records:
+            self.output("A record for", domain, ":", str(ip))
+            self.spf_ip_list.append(str(ip))
+      except dns.exception.DNSException:
+         pass
+   
+   def convert_mx_to_ipv4(self, domain):
+   # Convert MX records to IPs and adds them to the SPF master list
+      try:
+         mx_records = dns.resolver.query(domain, "MX")
+      except dns.exception.DNSException:
+         return
 
+      for record in mx_records:
+         mx = str(record).split(' ')
+         self.output("MX record found for ", domain, ": ", mx[1])
+         self.convert_domain_to_ipv4(mx[1])  
+   
+   def flatten_spf(self, domain):
+   # Recursively flatten the SPF record for the specified domain
+      self.output("--- Flattening:", domain, "---")
+      try:
+         txt_records = dns.resolver.query(domain, "TXT")
+      except dns.exception.DNSException:
+         self.output("No TXT records for:", domain)
+         return
 
-# Recursively flatten the SPF record for the specified domain
-def flatten_spf(domain):
-   print "--- Flattening:", domain, "---"
-   try:
-      txt_records = dns.resolver.query(domain, "TXT")
-   except dns.exception.DNSException:
-      print "No TXT records for:", domain
-      return
+      for record in txt_records:
+         self.output("TXT record for:", domain, ":", str(record))
+         fields = str(record)[1:-1].split(' ')
 
-   for record in txt_records:
-      print "TXT record for:", domain, ":", str(record)
-      fields = str(record)[1:-1].split(' ')
+         if re.match(r'v=spf1', fields[0]):
+            for field in fields:
+               self.parse_mechanism(field, domain)
 
-      if re.match(r'v=spf1', fields[0]):
-         for field in fields:
-            parse_mechanism(field, domain)
+   def flatten(self):
+   # Creates the initial flat SPF record with all the IP addresses.
+      self.flatten_spf(self.root_domain)
 
-
-# Parse the given mechansim, and dispatch it accordintly
-def parse_mechanism(mechanism, domain):
-   global all_mechanism
-   if re.match(r'^a$', mechanism):
-      convert_domain_to_ipv4(domain)
-   elif re.match(r'^mx$', mechanism):
-      convert_mx_to_ipv4(domain)
-   elif re.match(r'^a:.*$', mechanism):
-      match = re.match(r'^a:(.*)$', mechanism)
-      convert_domain_to_ipv4(match.group(1))
-   elif re.match(r'^ip4:.*$', mechanism):
-      match = re.match(r'^ip4:(.*)$', mechanism)
-      print "IPv4 address found for", domain, ":", match.group(1)
-      spf_ip_list.append(match.group(1))
-   elif re.match(r'^ip6:.*$', mechanism):
-      match = re.match(r'^ip6:(.*)$', mechanism)
-      print "IPv6 address found for", domain, ":", match.group(1)
-      spf_ip_list.append(match.group(1))
-   elif re.match(r'^ptr.*$', mechanism):
-      print "PTR found for", domain, ":", mechanism
-      spf_nonflat_mechanisms.append(mechanism)
-   elif re.match(r'^exists:$', mechanism):
-      print "Exists found for", domain, ":", mechanism
-      spf_nonflat_mechanisms.append(mechanism)
-   elif re.match(r'^redirect:$', mechanism):
-      print "Redirect found for", domain, ":", mechanism
-      spf_nonflat_mechanisms.append(mechanism)
-   elif re.match(r'^exp:$', mechanism):
-      print "EXP found for", domain, ":", mechanism
-      spf_nonflat_mechanisms.append(mechanism)
-   elif re.match(r'^.all$', mechanism):
-      if domain == root_domain:
-         match = re.match(r'^(.all)$', mechanism)
-         print "All found for", domain, ":", match.group(1)
-         all_mechanism = " " + str(match.group(1))
-   elif re.match(r'^include:.*$', mechanism):
-      match = re.match(r'^include:(.*)', mechanism)
-      flatten_spf(match.group(1)) # recursion
-
-
-# Convert A/AAAA records to IPs and adds them to the SPF master list
-def convert_domain_to_ipv4(domain):
-   a_records = []
-   aaaa_records = []
-
-   try:
-      a_records = dns.resolver.query(domain, "A")
-      for ip in a_records:
-         print "A record for", domain, ":", str(ip)
-         spf_ip_list.append(str(ip))
-   except dns.exception.DNSException:
-      pass
-
-   try:
-      aaaa_records = dns.resolver.query(domain, "AAAA")
-      for ip in aaaa_records:
-         print "A record for", domain, ":", str(ip)
-         spf_ip_list.append(str(ip))
-   except dns.exception.DNSException:
-      pass
-
-
-# Convert MX records to IPs and adds them to the SPF master list
-def convert_mx_to_ipv4(domain):
-   try:
-      mx_records = dns.resolver.query(domain, "MX")
-   except dns.exception.DNSException:
-      return
-
-   for record in mx_records:
-      mx = str(record).split(' ')
-      print "MX record found for ", domain, ": ", mx[1]
-      convert_domain_to_ipv4(mx[1])      
+      dedupe_spf_ip_list = list(set(self.spf_ip_list))
       
+      self.raw_flat_spf = "v=spfv1"
+      
+      ip_string = lambda ip: " ip6:%s" % ip if re.match(r'.*:.*', ip) else " ip4:%s" % ip
+      for ip in dedupe_spf_ip_list:
+         self.raw_flat_spf += ip_string(ip)
 
-if __name__ == "__main__": main()
+      for mechanism in self.spf_nonflat_mechanisms: 
+         self.raw_flat_spf += mechanism
+
+      self.raw_flat_spf += self.all_mechanism
+      self.split_records[self.root_domain] = self.raw_flat_spf
+
+   def structure_spf(self, count=1, block_limit=450):
+   # Ass UDP packets can only be 512 bytes, splits the flattened spf record into blocks smaller than the block limit
+      if not any([getsizeof(record) > block_limit for x, record in self.split_records.items() ]):
+         return
+      for domain, spf_record in dict(self.split_records).items():
+         while getsizeof(spf_record) > block_limit:
+            ip_list = spf_record.split(' ')
+            new_domain_record = 'spf%s.%s' % (count, self.root_domain)
+
+            if new_domain_record not in self.split_records.keys():
+               self.split_records[new_domain_record] = 'v=spf1'   
+            self.split_records[new_domain_record] += ' %s' % ip_list.pop(len(ip_list)-2)
+            
+            spf_record = ' '.join(ip_list)
+            self.split_records[domain] = spf_record
+
+      self.structure_spf(count=count+1) # <<--- Recursively reduce the size of each record
+      return
+
+   def add_includes(self):
+   # Adds the include:<ip> to each record where needed.
+      include = lambda r_list, value: r_list.insert(len(r_list)-1, value)
+      for domain, record in self.spf_records.items():
+         record_list = record.split(' ')
+         if 'spf' in domain.split('.')[0]:
+            record_num = int(domain.split('.')[0][-1:])
+            next_record = 'spf%s.%s' % (record_num+1,self.root_domain)
+            if next_record in self.spf_records.keys():
+               include(record_list, 'include:%s' % next_record)
+         else: 
+            next_record = 'include:spf1.%s' % self.root_domain
+            if len(self.spf_records.keys()) > 1:
+               include(record_list, next_record)
+         self.spf_records[domain] = ' '.join(record_list)
+      return
+
+   def Outputs(self):
+   # Prints out SPF record outputs
+      if self.json_output:
+         print(json.dumps(self.spf_records))
+         return
+      for domain, record in self.spf_records.items(): 
+         print('\n%s:\n%s\n' % (domain, record))
+
+
+if __name__ == "__main__":
+   if len(argv) > 1:
+      SPFlatten(
+            root_domain=argv[1].replace('www.', ''),
+            verbose=('-v' in argv),
+            json_output=('-j' in argv)
+         ).Outputs()
+   else:
+      print('\n Usage: %s [domain] [flags]\n\n-j\tjson output\n-v\tverbose output\n' % argv[0])
